15f0561ed4d34f0bc978f6aef3c2b7985a4ce5a0
nagachandu2003 <nagachanduthatha@gmail.com>
2024-05-20 19:44:03 +0530

    added video count

--- ytcm/src/Components/YTCMChannelItem/index.js
+++ ytcm/src/Components/YTCMChannelItem/index.js
@@ -3,7 +3,7 @@ import {Link} from 'react-router-dom'
 
 const YTCMChannelItem = (props) => {
     const {itemDetails} = props
-    const {channelName,channelDate,channelTime} = itemDetails
+    const {channelName,channelDate,channelTime,videos} = itemDetails
     const url = `https://www.youtube.com/@${channelName}` 
     return (
         <li className="ytmcchannel-list-item">
@@ -14,6 +14,7 @@ const YTCMChannelItem = (props) => {
             <Link to={`/video/${channelName}`}>
             <button style={{padding:'10px'}} type="button" className="view-Btn">Views</button>
             </Link>
+            <span style={{marginLeft:'10px'}}>Videos : {videos.length}</span>
             </div>
             <img style={{width:'70px',objectFit:'contain'}} className="user-img" src="https://imgs.search.brave.com/mVZUg9yLtbGziLSE-n97XJJN60bZQoIZN199kEK-RIE/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9jZG4t/aWNvbnMtcG5nLmZy/ZWVwaWsuY29tLzI1/Ni8xMzg0LzEzODQw/NjAucG5n" alt="img" />
             </div>
