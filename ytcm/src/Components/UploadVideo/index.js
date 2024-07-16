077fdd686370b863fb070595418ba7db86ce30c5
Naga Chandu Thatha <nagachanduthatha@gmail.com>
2024-07-16 17:08:38 +0530

    added uploadvideo

--- ytcm/src/Components/UploadVideo/index.js
+++ ytcm/src/Components/UploadVideo/index.js
@@ -20,7 +20,7 @@ const UploadVideo = () => {
         videoData.append("description", form.description);
 
         try {
-            const response = await axios.post(`http://localhost:3001/uploadvideo`, videoData, {
+            const response = await axios.post(`https://js-member-backend.vercel.app/uploadvideo`, videoData, {
                 headers: {
                     'Content-Type': 'multipart/form-data'
                 }
